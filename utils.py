7652ce541ab56dcebc485fee773e07674205bde5
krimtron <142521277+krimtron@users.noreply.github.com>
2023-12-14 13:56:31 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+from random import choice
+from models import User
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
